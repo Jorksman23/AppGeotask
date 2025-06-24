95c2474dc283216286e431615ca1aa0dc10e1d05
Jorge Luis <joluzamce2001@gmail.com>
2025-06-24 16:16:23 -0500

    Logout

--- router/AuthRouter.js
+++ router/AuthRouter.js
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import { registerUser } from '../controller/AuthController.js';
 import { validateRegister } from '../middleware/validatorMiddleware.js';
 import rateLimit from '../middleware/rateLimiter.js';
-import { loginUser, registerUser } from '../controllers/AuthController.js';
+import { loginUser} from '../controller/AuthController.js';
 const router = Router();
 
 router.post('/register', rateLimit, validateRegister, registerUser);
 router.post('/login', loginUser);
-
+router.post('/logout', (req, res) => {
+  return res.status(200).json({
+    mensaje: 'Sesión cerrada correctamente.'
+  });
+});
 // Agregaremos más rutas luego
 
 export default router;
